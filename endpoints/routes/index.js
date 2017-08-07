7d6342c83d24447f8dd92bd744a5364672ffaf36
Aida Khachatrian <aidakhachatrian@gmail.com>
2017-08-07 11:41:49 +0400

    created chat page contacts block

--- endpoints/routes/index.js
+++ endpoints/routes/index.js
@@ -1,32 +1,37 @@
 module.exports = [
-    {
-        url: '/',
-        method: 'get',
-        handler: './handlers/home'
-    },
-    {
-        url: '/signUp',
-        method: 'get',
-        handler: './handlers/signUp'
-    },
-    {
-        url: '/404',
-        method: 'get',
-        handler: './handlers/404'
-    },
-    {
-        url: '/myProfile',
-        method: 'post',
-        handler: './handlers/profilePage'
-    },
-    {
-        url: '/myProfile',
-        method: 'get',
-        handler: './handlers/profilePage'
-    },
-    {
-        url: '*',
-        method: 'get',
-        handler: './handlers/error'
-    },
+  {
+    url: '/',
+    method: 'get',
+    handler: './handlers/home'
+  },
+  {
+    url: '/signUp',
+    method: 'get',
+    handler: './handlers/signUp'
+  },
+  {
+    url: '/404',
+    method: 'get',
+    handler: './handlers/404'
+  },
+  {
+    url: '/myProfile',
+    method: 'post',
+    handler: './handlers/profilePage'
+  },
+  {
+    url: '/myProfile',
+    method: 'get',
+    handler: './handlers/profilePage'
+  },
+  {
+    url: '/messages',
+    method: 'get',
+    handler: './handlers/chat'
+  },
+  {
+    url: '*',
+    method: 'get',
+    handler: './handlers/error'
+  },
 ];
